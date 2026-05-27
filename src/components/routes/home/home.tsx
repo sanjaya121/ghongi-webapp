8d2dbc10e72055c7f69ec95b0413eed18f95bf12
Sanjaya Chaudhary <sanjaya121@gmail.com>
2026-05-26 21:29:20 -0500

    setup tailwind Css

---
+++ src/components/routes/home/home.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../../shared/navigation/navbar";
+
+const Home =()=>{
+
+    return(
+        <>
+           <Navbar/>
+
+        </>
+    )
+
+
+}
+
+export default Home;
---
+++ src/components/shared/navigation/navbar.tsx
@@ -0,0 +1,17 @@
+
+const Navbar =()=>{
+
+    return(
+
+        <div>
+            <h1> Nav Bar</h1>
+
+            <div className="mt-6 ...">mt-6</div>
+<div className="mr-4 w-96">mr-4</div>
+<div className="mb-8 ...">mb-8</div>
+<div className="ml-2 ...">ml-2</div>
+        </div>
+    )
+}
+
+export default Navbar
